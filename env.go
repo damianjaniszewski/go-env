79792962b601b9a054c5216967c1bab4a540f151
Damian Janiszewski <damian.janiszewski@gmail.com>
2017-12-11 07:42:42 +0100

    source code cleaning

--- env.go
+++ env.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
-        "strings"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func handler(w http.ResponseWriter, req *http.Request) {
-        fmt.Fprintf(w, "%+v: %+v\n", "STACKATO_APP_NAME", os.Getenv("STACKATO_APP_NAME"))
-        fmt.Fprintf(w, "%+v: %+v\n", "CF_INSTANCE_ADDR", os.Getenv("CF_INSTANCE_ADDR"))
-        fmt.Fprintf(w, "%+v: %+v\n", "CF_INSTANCE_INDEX", os.Getenv("CF_INSTANCE_INDEX"))
-        fmt.Fprintf(w, "%+v: %+v\n", "STACKATO_APP_ENV", os.Getenv("STACKATO_APP_ENV"))
-        fmt.Fprintf(w, "%+v: %+v\n", "STACKATO_SERVICES", os.Getenv("STACKATO_SERVICES"))
-        fmt.Fprintln(w, "")
+	fmt.Fprintf(w, "%+v: %+v\n", "STACKATO_APP_NAME", os.Getenv("STACKATO_APP_NAME"))
+	fmt.Fprintf(w, "%+v: %+v\n", "CF_INSTANCE_ADDR", os.Getenv("CF_INSTANCE_ADDR"))
+	fmt.Fprintf(w, "%+v: %+v\n", "CF_INSTANCE_INDEX", os.Getenv("CF_INSTANCE_INDEX"))
+	fmt.Fprintf(w, "%+v: %+v\n", "STACKATO_APP_ENV", os.Getenv("STACKATO_APP_ENV"))
+	fmt.Fprintf(w, "%+v: %+v\n", "STACKATO_SERVICES", os.Getenv("STACKATO_SERVICES"))
+	fmt.Fprintln(w, "")
 
-        fmt.Printf("%+v\n", req)
-        fmt.Fprintln(w, strings.Join(os.Environ(), "\n"))
-        if req.URL.Path == "/crash" {
-                os.Exit(1)
-        }
+	fmt.Printf("%+v\n", req)
+	fmt.Fprintln(w, strings.Join(os.Environ(), "\n"))
+	if req.URL.Path == "/crash" {
+		os.Exit(1)
+	}
 }
 
 func main() {
-        http.HandleFunc("/", handler)
-        addr := ":" + os.Getenv("PORT")
-        fmt.Printf("Listening on %v\n", addr)
-        log.Fatal(http.ListenAndServe(addr, nil))
+	http.HandleFunc("/", handler)
+	addr := ":" + os.Getenv("PORT")
+	fmt.Printf("Listening on %v\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
